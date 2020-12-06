869aea27639dace4983f3565db1b5b759c5bc6dc
Becky <bckyjns@gmail.com>
2020-12-06 20:11:18 +0000

    💄 migrate heading component

---
+++ src/components/Heading/Heading.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components/macro';
+
+export const Heading = styled.h1`
+  margin-top: 0;
+  padding: var(--spacing-md) var(--gutter-page);
+  color: var(--color-white);
+  background-color: var(--color-secondary);
+`;
