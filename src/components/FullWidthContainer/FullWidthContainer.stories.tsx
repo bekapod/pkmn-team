6aa79ac39e8710ab2060ecd86389f41cd6770bc3
Becky <bckyjns@gmail.com>
2020-12-06 20:04:20 +0000

    💄 migrate full width container

---
+++ src/components/FullWidthContainer/FullWidthContainer.stories.tsx
@@ -0,0 +1,10 @@
+import { FullWidthContainer } from './FullWidthContainer';
+
+export default {
+  title: 'Components/FullWidthContainer',
+  component: FullWidthContainer
+};
+
+export const Standard = (): JSX.Element => (
+  <FullWidthContainer>A section of content</FullWidthContainer>
+);
---
+++ src/components/FullWidthContainer/FullWidthContainer.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components/macro';
+
+export const FullWidthContainer = styled.div`
+  margin-left: var(--gutter-page);
+  margin-right: var(--gutter-page);
+`;
