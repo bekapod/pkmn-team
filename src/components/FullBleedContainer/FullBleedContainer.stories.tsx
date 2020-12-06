0ea4e56a0c90ff5068361199af4e762e07d983b9
Becky <bckyjns@gmail.com>
2020-12-06 20:04:29 +0000

    💄 migrate full bleed container

---
+++ src/components/FullBleedContainer/FullBleedContainer.stories.tsx
@@ -0,0 +1,10 @@
+import { FullBleedContainer } from './FullBleedContainer';
+
+export default {
+  title: 'Components/FullBleedContainer',
+  component: FullBleedContainer
+};
+
+export const Standard = (): JSX.Element => (
+  <FullBleedContainer>A section of content</FullBleedContainer>
+);
---
+++ src/components/FullBleedContainer/FullBleedContainer.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components/macro';
+
+export const FullBleedContainer = styled.div`
+  margin-right: calc(var(--gutter-page) * -1);
+  margin-left: calc(var(--gutter-page) * -1);
+`;
