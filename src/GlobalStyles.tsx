f1e0527591d9d1bd7280253b1a425823b64dd3cc
Becky <bckyjns@gmail.com>
2020-12-06 17:33:14 +0000

    💄 migrate card component

--- src/GlobalStyles.tsx
+++ src/GlobalStyles.tsx
@@ -1,10 +1,98 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
+  :root {
+    --color-primary: #e84118;
+    --color-primary-dark: #c23616;
+    --color-secondary: #273c75;
+    --color-secondary-dark: #192a56;
+    --color-selection: #fbc531;
+    --color-tertiary: #fbc531;
+
+    --color-gray: #7f8fa6;
+    --color-gray-dark: #353b48;
+    --color-gray-darker: #2f3640;
+    --color-gray-light: #dcdde1;
+    --color-white: #f5f6fa;
+
+    --color-error: #e84118;
+
+    --color-bug: #A8B820;
+    --color-dark: #705848;
+    --color-dragon: #7038F8;
+    --color-electric: #F8D030;
+    --color-fairy: #EE99AC;
+    --color-fighting: #C03028;
+    --color-fire: #F08030;
+    --color-flying: #A890F0;
+    --color-ghost: #705898;
+    --color-grass: #78C850;
+    --color-ground: #E0C068;
+    --color-ice: #98D8D8;
+    --color-normal: #A8A878;
+    --color-poison: #A040A0;
+    --color-psychic: #F85888;
+    --color-rock: #B8A038;
+    --color-shadow: #604E82;
+    --color-steel: #B8B8D0;
+    --color-unknown: #68A090;
+    --color-water: #6890F0;
+
+    --color-physical: #c23616;
+    --color-special: #192a56;
+    --color-status: #7f8fa6;
+
+    --font-base: 'brandon-grotesque', sans-serif;
+
+    --baseline: 1.5;
+    line-height: var(--baseline);
+
+    --font-size-base: 1.125rem;
+    --font-size-xxs: 0.3125rem;
+    --font-size-xs: 0.875rem;
+    --font-size-sm: 1.125rem;
+    --font-size-md: 1.375rem;
+    --font-size-lg: 2rem;
+    --font-size-xl: 3rem;
+    --font-size-xxl: 4rem;
+
+    --spacing-xxs: calc(var(--baseline) * 0.125rem);
+    --spacing-xs: calc(var(--baseline) * 0.25rem);
+    --spacing-sm: calc(var(--baseline) * 0.5rem);
+    --spacing-md: calc(var(--baseline) * 1rem);
+    --spacing-lg: calc(var(--baseline) * 2rem);
+    --spacing-xl: calc(var(--baseline) * 3rem);
+    --spacing-xxl: calc(var(--baseline) * 4rem);
+
+    --gutter-grid: 32px;
+    --gutter-page: 40px;
+
+    --border-radius: 0.75rem;
+    --border-radius-sm: 0.25rem;
+
+    --zig-zag: 2rem;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  html {
     box-sizing: border-box;
+    overflow-x: hidden;
+    color: var(--color-gray-dark);
+    font-family: var(--font-base);
+    font-weight: 400;
+    line-height: var(----rhythm-unit);
+    background-color: var(--color-gray-light);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  ::selection {
+    background-color: var(--color-selection);
+    color: var(--color-gray-dark);
   }
 `;
 
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,69 @@
+import styled from 'styled-components/macro';
+import { Type } from '~/generated/graphql';
+import { zoomIn } from '~lib/animations';
+import { getTypeGradient } from '~lib/gradients';
+
+type CardHeaderProps = {
+  types: Pick<Type, 'name' | 'slug'>[];
+};
+
+export const CardLink = styled.a`
+  ${zoomIn}
+  color: initial;
+  text-decoration: none;
+`;
+
+export const CardWrapper = styled.article`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+export const CardHeader = styled.header<CardHeaderProps>`
+  overflow: hidden;
+  margin-bottom: calc(var(--spacing-xl) * -1);
+  padding: 0 var(--spacing-md) var(--spacing-lg);
+  color: var(--color-white);
+  text-shadow: 0 1px 0px var(--color-gray-darker),
+    1px 0 0px var(--color-gray-dark), 1px 2px 0px var(--color-gray-darker),
+    2px 1px 0px var(--color-gray-dark), 2px 3px 0px var(--color-gray-darker),
+    3px 2px 0px var(--color-gray-dark);
+  background-image: ${({ types }) => getTypeGradient(types)};
+  border-radius: var(--border-radius) 0 0 0;
+`;
+
+export const CardHeading = styled.h2`
+  overflow: hidden;
+  margin: var(--spacing-lg) 0;
+  text-align: center;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+export const CardContent = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  flex: 1 0 auto;
+  margin: var(--spacing-md) var(--spacing-sm) 0;
+  padding: 0 var(--spacing-sm);
+  background-color: var(--color-white);
+  border-radius: 0 0 var(--border-radius) 0;
+  &::before {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: calc(var(--zig-zag) / 2 * -1);
+    width: 100%;
+    height: calc(var(--zig-zag) / 2);
+    background: linear-gradient(
+        -45deg,
+        var(--color-white) var(--zig-zag),
+        transparent 0
+      ),
+      linear-gradient(45deg, var(--color-white) var(--zig-zag), transparent 0);
+    background-repeat: repeat-x;
+    background-position: left top;
+    background-size: var(--zig-zag) 46px;
+  }
+`;
