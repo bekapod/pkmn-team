ce25abfe5264d68b7ee3098ffea09ae9bf4a9873
Becky <bckyjns@gmail.com>
2020-12-17 20:23:55 +0000

    💄 do card meta

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -2,8 +2,8 @@ import styled from 'styled-components/macro';
 
 export const Label = styled.span`
   color: var(--color-gray);
-  font-size: var(--font-size-xs);
-  line-height: var(--spacing-md);
+  font-size: var(--font-size-sm);
+  line-height: var(--line-height-sm);
   font-weight: 900;
   text-transform: uppercase;
 `;
