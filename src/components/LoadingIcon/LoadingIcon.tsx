abde0f7484ee42e4e82b3089f171b156796b0efb
Becky <bckyjns@gmail.com>
2020-12-18 23:31:21 +0000

    💄 allow loading icon to be centred

--- src/components/LoadingIcon/LoadingIcon.tsx
+++ src/components/LoadingIcon/LoadingIcon.tsx
@@ -5,16 +5,20 @@ import styles from './LoadingIcon.module.css';
 export type LoadingIconProps = {
   spinner?: boolean;
   small?: boolean;
+  centered?: boolean;
 };
 
 const loadingText = 'Loading';
 
 export const LoadingIcon: FunctionComponent<
   ComponentPropsWithRef<'div'> & LoadingIconProps
-> = ({ spinner, small, className, ...props }) =>
+> = ({ spinner, small, centered, className, ...props }) =>
   spinner ? (
     <div
-      className={cx(className, styles.spinner, { [styles['is-small']]: small })}
+      className={cx(className, styles.spinner, {
+        [styles['is-small']]: small,
+        [styles['is-centered']]: centered
+      })}
       data-testid="loading-spinner"
       role="img"
       aria-label={loadingText}
