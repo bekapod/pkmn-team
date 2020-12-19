8d2255d8708647c4d37316bf50aea2b5ce2404f6
Becky <bckyjns@gmail.com>
2020-12-19 16:01:53 +0000

    ✨ can add icons to buttons

--- src/components/Cta/Cta.stories.tsx
+++ src/components/Cta/Cta.stories.tsx
@@ -1,4 +1,5 @@
 import { Meta } from '@storybook/react/types-6-0';
+import { BiHappyHeartEyes as Icon } from 'react-icons/bi';
 import { CtaInternalLink, CtaButton, CtaProps } from './Cta';
 
 export default {
@@ -15,3 +16,8 @@ export const Button = (args: CtaProps): JSX.Element => (
   <CtaButton {...args}>Create Team</CtaButton>
 );
 Button.args = { disabled: false };
+
+export const WithIcon = (args: CtaProps): JSX.Element => (
+  <CtaButton {...args}>Create Team</CtaButton>
+);
+WithIcon.args = { icon: Icon };
