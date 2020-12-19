ba4cbb78b9ae7faa09362ecde38b3885435e537f
Becky <bckyjns@gmail.com>
2020-12-19 15:13:40 +0000

    💄 update icon on add button

--- src/components/TeamView/TeamView.tsx
+++ src/components/TeamView/TeamView.tsx
@@ -14,6 +14,7 @@ import {
   Draggable,
   DropResult
 } from 'react-beautiful-dnd';
+import { BiPlusMedical as Plus } from 'react-icons/bi';
 import { v4 as uuid } from 'uuid';
 import useDeepCompareEffect from 'use-deep-compare-effect';
 import { PokemonSearch } from '../PokemonSearch';
@@ -205,7 +206,7 @@ export const TeamView: FunctionComponent<TeamViewProps> = memo(
                               className={styles['add-button']}
                               aria-label="Add new pokemon to team"
                             >
-                              +
+                              <Plus className={styles['add-icon']} />
                             </span>
                           </div>
                         )}
