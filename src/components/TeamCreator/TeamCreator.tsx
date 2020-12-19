7b299a35fb4ba5da8a373cc83a83b9e1b69bfd8d
Becky <bckyjns@gmail.com>
2020-12-19 23:17:28 +0000

    ✨ integrate team creator as proper form

--- src/components/TeamCreator/TeamCreator.tsx
+++ src/components/TeamCreator/TeamCreator.tsx
@@ -1,28 +1,62 @@
 import type { ComponentPropsWithoutRef, FunctionComponent } from 'react';
 import cx from 'classnames';
+import { useForm } from 'react-hook-form';
 import { CenteredRow } from '../CenteredRow';
 import { CtaButton } from '../Cta';
 import { FormField } from '../FormField';
 import { GiantInput } from '../GiantInput';
 import styles from './TeamCreator.module.css';
 
-export type TeamCreatorProps = ComponentPropsWithoutRef<'form'>;
+export type TeamCreatorFormValues = {
+  'team-name': string;
+};
+
+export type TeamCreatorProps = ComponentPropsWithoutRef<'form'> & {
+  defaultValues?: Partial<TeamCreatorFormValues>;
+  createTeam?: (values: TeamCreatorFormValues) => void;
+};
 
 export const TeamCreator: FunctionComponent<TeamCreatorProps> = ({
   className,
+  defaultValues,
+  createTeam,
   ...props
 }) => {
+  const { register, handleSubmit, errors } = useForm<TeamCreatorFormValues>({
+    defaultValues
+  });
+  const error = errors['team-name']?.message;
+
+  const onSubmit = (values: TeamCreatorFormValues) => createTeam?.(values);
+
   return (
     <CenteredRow
       as="form"
       stackVertically
       className={cx(styles.form, className)}
+      onSubmit={handleSubmit(onSubmit)}
       {...props}
     >
-      <FormField label="Team name" id="team-name" className={styles.field}>
-        <GiantInput id="team-name" />
+      <FormField
+        label="Team name"
+        id="team-name"
+        className={styles.field}
+        error={error}
+      >
+        <GiantInput
+          id="team-name"
+          name="team-name"
+          isInvalid={!!error}
+          aria-required="true"
+          ref={register({
+            required: {
+              value: true,
+              message: 'You must name your team'
+            }
+          })}
+        />
       </FormField>
-      <CtaButton>Create team</CtaButton>
+      <CtaButton type="submit">Create team</CtaButton>
     </CenteredRow>
   );
 };
