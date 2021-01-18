6d70c9eba9a260d8f2772d9401e7b44c7fb1493e
Becky <bckyjns@gmail.com>
2021-01-18 21:51:13 +0000

    ✅ centered row tests

---
+++ src/components/CenteredRow/CenteredRow.test.tsx
@@ -0,0 +1,29 @@
+import { render } from '@testing-library/react';
+import { CenteredRow } from '.';
+
+describe(CenteredRow, () => {
+  it("renders it's children in a div", () => {
+    const { getByText } = render(<CenteredRow>hello</CenteredRow>);
+    expect(getByText('hello')).toBeInTheDocument();
+    expect(getByText('hello').tagName).toBe('DIV');
+  });
+
+  it('adds additional class names', () => {
+    const { getByText } = render(
+      <CenteredRow className="some-custom-class">hello</CenteredRow>
+    );
+    expect(getByText('hello')).toHaveClass('some-custom-class');
+  });
+
+  it('can stack content vertically', () => {
+    const { getByText } = render(
+      <CenteredRow stackVertically>hello</CenteredRow>
+    );
+    expect(getByText('hello')).toHaveClass('is-vertical');
+  });
+
+  it('can render as a different element', () => {
+    const { getByText } = render(<CenteredRow as="main">hello</CenteredRow>);
+    expect(getByText('hello').tagName).toBe('MAIN');
+  });
+});
