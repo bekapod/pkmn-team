13ab0bd03de4765e665bfef44348b4f0698e03c8
Becky <bckyjns@gmail.com>
2021-02-13 23:51:41 +0000

    💄 pokemon grid

--- src/components/PokemonGrid/PokemonGrid.tsx
+++ src/components/PokemonGrid/PokemonGrid.tsx
@@ -1,8 +1,19 @@
 import type { FunctionComponent, ComponentPropsWithRef } from 'react';
 import cx from 'classnames';
-import styles from './PokemonGrid.module.css';
 
 export const PokemonGrid: FunctionComponent<ComponentPropsWithRef<'div'>> = ({
   className,
   ...props
-}) => <div className={cx(styles.container, className)} {...props} />;
+}) => (
+  <div
+    className={cx(
+      'grid',
+      'gap-5',
+      'grid-cols-1',
+      'w-full',
+      'md:auto-cols-min',
+      className
+    )}
+    {...props}
+  />
+);
