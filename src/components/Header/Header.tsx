89cbd0e7cff011df31f86fa23b3172f4994ff05e
Becky <bckyjns@gmail.com>
2021-02-13 22:10:26 +0000

    💄 header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,6 @@ import type {
 } from 'react';
 import Link from 'next/link';
 import cx from 'classnames';
-import styles from './Header.module.css';
 import { Pokeball } from '../Pokeball';
 
 export type HeaderProps = ComponentPropsWithRef<'header'> & {
@@ -17,12 +16,40 @@ export const Header: FunctionComponent<HeaderProps> = ({
   title,
   ...props
 }) => (
-  <header className={cx(styles.container, className)} {...props}>
+  <header
+    className={cx(
+      'flex',
+      'items-center',
+      'mb-5',
+      'py-3',
+      'px-4',
+      'bg-indigo-700',
+      'md:py-3',
+      'md:px-6',
+      className
+    )}
+    {...props}
+  >
     <Link href="/">
-      <a title="Dashboard" className={styles['logo-link']}>
-        <Pokeball role="presentation" className={styles.logo} />
+      <a
+        title="Dashboard"
+        className={cx(
+          'block',
+          'mr-4',
+          'transition-transform',
+          'duration-300',
+          'ease-out',
+          'transform-gpu',
+          'scale-100',
+          'hover:scale-110',
+          'focus:scale-110'
+        )}
+      >
+        <Pokeball role="presentation" className={cx('block', 'w-8', 'h-8')} />
       </a>
     </Link>
-    <h1 className={styles.title}>{title}</h1>
+    <h1 className={cx('m-0', 'text-xl', 'font-black', 'text-white')}>
+      {title}
+    </h1>
   </header>
 );
