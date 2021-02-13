c5c7ef05c4b15bea6c9be88218cb9ea04040ec39
Becky <bckyjns@gmail.com>
2021-02-13 22:10:32 +0000

    💄 inline list

--- src/components/InlineList/InlineList.tsx
+++ src/components/InlineList/InlineList.tsx
@@ -1,8 +1,12 @@
 import type { FunctionComponent, ComponentPropsWithRef } from 'react';
 import cx from 'classnames';
-import styles from './InlineList.module.css';
 
 export const InlineList: FunctionComponent<ComponentPropsWithRef<'ul'>> = ({
   className,
   ...props
-}) => <ul className={cx(styles.base, className)} {...props} />;
+}) => (
+  <ul
+    className={cx('flex', 'flex-wrap', 'children:not-last:mr-3', className)}
+    {...props}
+  />
+);
