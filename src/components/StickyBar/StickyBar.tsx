6e47f4a04ffae98060f296954a8fc9657fbce95f
Becky <bckyjns@gmail.com>
2021-02-13 23:52:11 +0000

    💄 sticky bar

--- src/components/StickyBar/StickyBar.tsx
+++ src/components/StickyBar/StickyBar.tsx
@@ -1,8 +1,7 @@
 import type { FunctionComponent, ComponentPropsWithRef } from 'react';
 import cx from 'classnames';
-import styles from './StickyBar.module.css';
 
 export const StickyBar: FunctionComponent<ComponentPropsWithRef<'div'>> = ({
   className,
   ...props
-}) => <div className={cx(styles.container, className)} {...props} />;
+}) => <div className={cx('sticky-bar', className)} {...props} />;
