45bc18ce5114b7ca4412f9a64256d15940f71651
Becky <bckyjns@gmail.com>
2021-02-13 17:08:39 +0000

    💄 card meta

--- src/components/CardMeta/CardMeta.tsx
+++ src/components/CardMeta/CardMeta.tsx
@@ -1,6 +1,6 @@
 import type { FunctionComponent } from 'react';
+import classNames from 'classnames';
 import { Label } from '../Label';
-import styles from './CardMeta.module.css';
 
 export type CardMetaProps = {
   id: string;
@@ -14,13 +14,50 @@ export const CardMeta: FunctionComponent<CardMetaProps> = ({
   id,
   items = []
 }) => (
-  <dl className={styles.wrapper} data-testid={`card-meta-${id}`}>
-    {items.map(({ label, value }) => (
-      <div className={styles.item} key={label}>
-        <Label className={styles.label} as="dt">
+  <dl className={classNames('flex', 'my-4')} data-testid={`card-meta-${id}`}>
+    {items.map(({ label, value }, idx) => (
+      <div
+        className={classNames(
+          'relative',
+          'flex',
+          'flex-col',
+          'flex-grow',
+          'flex-shrink-0',
+          'items-center',
+          'text-center'
+        )}
+        key={label}
+      >
+        <Label className={classNames('mb-1')} as="dt">
           {label}
         </Label>
-        <dd className={styles.value}>{value}</dd>
+        <dd
+          className={classNames(
+            'm-0',
+            'text-indigo-800',
+            'text-lg',
+            'font-black'
+          )}
+        >
+          {value}
+        </dd>
+        {idx !== items.length - 1 && (
+          <span
+            role="presentation"
+            className={classNames(
+              'block',
+              'absolute',
+              '-right-1',
+              'top-1/2',
+              'w-2',
+              'h-5',
+              'bg-indigo-100',
+              '-translate-y-1/2',
+              'transform-gpu',
+              'rounded-xl'
+            )}
+          />
+        )}
       </div>
     ))}
   </dl>
